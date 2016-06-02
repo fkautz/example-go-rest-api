5cabedaaf0ac776fec4d84acb500f10b43d95107
Frederick F. Kautz IV <fkautz@redhat.com>
2016-06-01 22:14:16 -0700

    Adding additional ws example and populating readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # example-go-rest-api
+
+```sh
+docker run -p 8080:8080 fkautz/example-go-rest-api
+```
+
+* http://localhost:8080/
+* http://localhost:8080/hello/{{world}}
+* http://localhost:8080/time
+
+time example is a modified example from
+github.com/gorilla/websocket/examples/echo which emits time on a regular basis
+rather than echoing back.
--- main.go
+++ main.go
@@ -11,6 +11,11 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", structuredHandler).Methods("GET")
 	r.HandleFunc("/hello/{person}", helloHandler).Methods("GET")
+	r.HandleFunc("/time", echoHandler).Methods("GET")
+	r.HandleFunc("/ws/time", wsHandler).Methods("GET")
 	http.Handle("/", r)
-	http.ListenAndServe("0.0.0.0:8080", r)
+	err := http.ListenAndServe("0.0.0.0:8080", r)
+	if err != nil {
+		log.Println(err)
+	}
 }
